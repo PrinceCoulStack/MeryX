9c511b058e8d1abedefbe678a6506a57a1841ca6
Amadou Coulibaly <coulibalyamadou7493@gmail.com>
2026-09-23 04:19:39 +0000

    Fix Symfony cache permissions in Docker

--- meryX/docker-entrypoint.sh
+++ meryX/docker-entrypoint.sh
@@ -11,8 +11,8 @@ if [ -n "$JWT_PUBLIC_KEY" ]; then
     printf '%s\n' "$JWT_PUBLIC_KEY" > /var/www/html/config/jwt/public.pem
 fi
 
-chown -R www-data:www-data /var/www/html/var /var/www/html/config/jwt
-
 php bin/console cache:clear --env=prod --no-debug
+
+chown -R www-data:www-data /var/www/html/var /var/www/html/config/jwt
 
 exec apache2-foreground
